aa30bf1a432c0346e34790ca99188f4bbc1fc35b
Davi de Oliveira <odavi278@gmail.com>
2024-01-02 16:54:28 -0300

    Finalizando a aplicacao

--- Menu.cs
+++ Menu.cs
@@ -15,6 +15,7 @@ namespace EditorHTML
       WriteOptions();
 
       var option = short.Parse(Console.ReadLine());
+      HandleMenuOption(option);
 
     }
     public static void DrawScreen()
@@ -55,6 +56,24 @@ namespace EditorHTML
       Console.WriteLine("0 - Sair");
       Console.SetCursorPosition(3, 10);
       Console.Write("Opçâo: ");
+
+    }
+
+    public static void HandleMenuOption(short option)
+    {
+      switch (option)
+      {
+        case 1: Editor.Show(); break;
+        case 2: Console.WriteLine("View"); break;
+        case 0:
+          {
+            Console.Clear();
+            Environment.Exit(0);
+            break;
+          }
+        default: Show(); break;
+
+      }
 
     }
   }
