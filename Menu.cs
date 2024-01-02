816a19100ed81b3a154a3e332053bc87df2ac234
Davi de Oliveira <odavi278@gmail.com>
2024-01-02 15:54:03 -0300

    Criando o menu de opcoes

--- Menu.cs
+++ Menu.cs
@@ -3,13 +3,59 @@ using System.Drawing;
 
 namespace EditorHTML
 {
-    public static class Menu
+  public static class Menu
+  {
+    public static void Show()
     {
-        public static void Show()
-        {
-          Console.Clear();
-          Console.BackgroundColor = ConsoleColor.Blue;
-          Console.BackgroundColor = ConsoleColor.Black;
-        }
+      Console.Clear();
+      Console.BackgroundColor = ConsoleColor.Blue;
+      Console.BackgroundColor = ConsoleColor.Black;
+
+      DrawScreen();
+      WriteOptions();
+
+      var option = short.Parse(Console.ReadLine());
+
     }
+    public static void DrawScreen()
+
+    {
+      Console.Write("+");
+      for (int i = 0; i <= 30; i++)
+        Console.Write("-");
+
+      Console.Write("+");
+      Console.Write("\n");
+
+      for (int lines = 0; lines <= 10; lines++)
+      {
+        Console.Write("|");
+        for (int i = 0; i <= 30; i++)
+          Console.Write(" ");
+        Console.Write("|");
+        Console.Write("\n");
+
+      }
+
+
+    }
+    public static void WriteOptions()
+    {
+      Console.SetCursorPosition(3, 2);
+      Console.WriteLine("Editor HTML");
+      Console.SetCursorPosition(3, 3);
+      Console.WriteLine("====================");
+      Console.SetCursorPosition(3, 4);
+      Console.WriteLine("Selecione uma opção abaixo");
+      Console.SetCursorPosition(3, 6);
+      Console.WriteLine("1 - Novo arquivo");
+      Console.SetCursorPosition(3, 7);
+      Console.WriteLine("2 - Abrir");
+      Console.SetCursorPosition(3, 9);
+      Console.WriteLine("0 - Sair");
+      Console.SetCursorPosition(3, 10);
+      Console.Write("Opçâo: ");
+
+    }
+  }
 }
